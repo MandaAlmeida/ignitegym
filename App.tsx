a9eb236b1c13dc6e696cc5b161c6c9f6261b01ec
amanda almeida <manda.fonsecaff@gmail.com>
2024-11-14 20:23:19 -0300

    Adiciona navegação ao aplicativo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
-import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
 
+import { Routes } from '@routes/index';
+import { Loading } from '@components/Loading';
 
 
 export default function App() {
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <GluestackUIProvider config={config}>
-      {fontsLoaded ? <SignIn /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
       <StatusBar style="light" backgroundColor="transparent" translucent />
     </GluestackUIProvider>
   );
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,11 +4,18 @@ import BackgroundImage from "@assets/background.png";
 import Logo from "@assets/logo.svg"
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import { useNavigation } from "@react-navigation/native";
 
 export function SignUp() {
+    const navigation = useNavigation();
+
+    function handleGoBack() {
+        navigation.goBack()
+    }
+
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
-            <VStack flex={1} bg="$gray700">
+            <VStack flex={1}>
                 <Image
                     source={BackgroundImage}
                     alt="Imagem de pessoas treinando"
@@ -31,7 +38,7 @@ export function SignUp() {
                         <Button title="Criar e acessar" />
                     </Center>
 
-                    <Button title="Voltar para o login" variant="outline" mt="$12" />
+                    <Button title="Voltar para o login" variant="outline" mt="$12" onPress={handleGoBack} />
                 </VStack>
             </VStack>
         </ScrollView>
